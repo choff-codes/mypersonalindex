84f6c0573f60c8229d546752614ae63f0184d67f
mypersonalindex <mypersonalindex@724262da-e012-11dd-ab27-75d9513d8d6c>
2009-01-11 21:32:54 +0000

    Initial code import.

---
+++ trunk/MyPersonalIndex/WinForms/frmPortfolios.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using System.Data.SqlServerCe;
+
+namespace MyPersonalIndex
+{
+
+    public partial class frmPortfolios : Form
+    {
+        public struct PortfolioRetValues
+        {
+            public int ID;
+            public string PortfolioName;
+            public bool Dividends;
+            public double NAVStart;
+            public int AAThreshold;
+            public DateTime StartDate;
+            public int CostCalc; 
+        }
+
+        public PortfolioRetValues PortfolioReturnValues { get { return _PortfolioReturnValues; } }
+        
+        private Queries SQL = new Queries();
+        private int Portfolio;
+        private PortfolioRetValues _PortfolioReturnValues = new PortfolioRetValues();
+        private MonthCalendar IndexDate;
+
+        public frmPortfolios(int PortfolioID, string sPortfolio, DateTime DataStartDate)
+        {
+            InitializeComponent();
+            Portfolio = PortfolioID;
+            this.Text = (string.IsNullOrEmpty(sPortfolio) ? "New Portfolio" : sPortfolio) + " Properties";
+            IndexDate = new MonthCalendar { MaxSelectionCount = 1, MaxDate = DateTime.Today, MinDate = DataStartDate, SelectionStart = DataStartDate};
+        }
+
+        private void frmPortfolios_Load(object sender, EventArgs e)
+        {
+            if (SQL.Connection == ConnectionState.Closed)
+            {
+                DialogResult = DialogResult.Cancel;
+                return;
+            }
+    
+            cmbCost.SelectedIndex = 0;
+            
+            ToolStripControlHost host = new ToolStripControlHost(IndexDate);
+            mnuDate.Items.Insert(0, host);
+            IndexDate.DateSelected += new DateRangeEventHandler(Date_Change);
+
+            SqlCeResultSet rs = SQL.ExecuteResultSet(Queries.Common_GetPortfolioAttributes(Portfolio));
+
+            try
+            {
+                if (rs.HasRows)
+                {
+                    rs.ReadFirst();
+
+                    txtName.Text = rs.GetString(rs.GetOrdinal("Name"));
+                    chkDiv.Checked = rs.GetSqlBoolean(rs.GetOrdinal("Dividends")).IsTrue;
+                    txtValue.Text = string.Format("{0:C}", rs.GetDecimal(rs.GetOrdinal("NavStartValue")));
+                    numAA.Value = rs.GetInt32(rs.GetOrdinal("AAThreshold"));
+                    cmbCost.SelectedIndex = rs.GetInt32(rs.GetOrdinal("CostCalc"));
+                    IndexDate.SetDate(rs.GetDateTime(rs.GetOrdinal("StartDate")));
+                }
+            }
+            finally
+            {
+                rs.Close();
+            }
+            btnDate.Text = IndexDate.SelectionStart.ToShortDateString();
+        }
+
+        private void Date_Change(object sender, DateRangeEventArgs e)
+        {
+            mnuDate.Close();
+            btnDate.Text = IndexDate.SelectionStart.ToShortDateString();
+        }
+
+        private void frmPortfolios_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            SQL.Dispose();
+        }
+
+        private void cmdOK_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtName.Text))
+            {
+                MessageBox.Show("Set a name before saving!");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(txtValue.Text))
+            {
+                MessageBox.Show("NAV Start Value must be number!");
+                return;
+            }
+
+            try
+            {
+                Double.Parse(txtValue.Text, System.Globalization.NumberStyles.Currency);
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("NAV Start Value must be number!");
+                return;
+            }
+
+            if (Portfolio == -1)
+            {
+                SQL.ExecuteNonQuery(Queries.Portfolio_InsertPortfolio(txtName.Text, chkDiv.Checked,
+                    Double.Parse(txtValue.Text, System.Globalization.NumberStyles.Currency), cmbCost.SelectedIndex,
+                    Convert.ToInt32(numAA.Value), Convert.ToDateTime(btnDate.Text)));
+                Portfolio = Convert.ToInt32(SQL.ExecuteScalar(Queries.Common_GetIdentity()));
+            }
+            else
+                SQL.ExecuteNonQuery(Queries.Portfolio_UpdatePortfolio(Portfolio, txtName.Text, chkDiv.Checked,
+                    Double.Parse(txtValue.Text, System.Globalization.NumberStyles.Currency), cmbCost.SelectedIndex,
+                    Convert.ToInt32(numAA.Value), Convert.ToDateTime(btnDate.Text)));
+
+            _PortfolioReturnValues.ID = Portfolio;
+            _PortfolioReturnValues.PortfolioName = txtName.Text;
+            _PortfolioReturnValues.Dividends = chkDiv.Checked;
+            _PortfolioReturnValues.AAThreshold = Convert.ToInt32(numAA.Value);
+            _PortfolioReturnValues.CostCalc = cmbCost.SelectedIndex;
+            _PortfolioReturnValues.NAVStart = Double.Parse(txtValue.Text, System.Globalization.NumberStyles.Currency);
+            _PortfolioReturnValues.StartDate = Convert.ToDateTime(btnDate.Text);
+            DialogResult = DialogResult.OK;
+                
+        }
+
+        private void txtValue_Leave(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!String.IsNullOrEmpty(txtValue.Text))
+                    txtValue.Text = string.Format("{0:C}", Convert.ToDouble(txtValue.Text));
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Invalid format, must be a number!", "Invalid Data", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void txtValue_Enter(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtValue.Text))
+                txtValue.Text = Double.Parse(txtValue.Text, System.Globalization.NumberStyles.Currency).ToString();
+        }
+
+        private void btnDate_Click(object sender, EventArgs e)
+        {
+            mnuDate.Show(btnDate, 0, btnDate.Height);
+        }
+
+        private void cmdCancel_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+        }
+    }
+}
---
+++ trunk/MyPersonalIndex/WinForms/frmTickers.cs
@@ -0,0 +1,358 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using System.Data.SqlServerCe;
+
+namespace MyPersonalIndex
+{
+    public partial class frmTickers : Form
+    {
+
+        public struct TickerRetValues
+        {
+            public DateTime MinDate;
+            public bool Changed;
+        }
+
+        public TickerRetValues TickerReturnValues { get { return _TickerReturnValues; } }
+
+        private Queries SQL = new Queries();
+        private int PortfolioID;
+        private int TickerID;
+        private bool Pasted;
+        private const int DateColumn = 0;
+        private TickerRetValues _TickerReturnValues = new TickerRetValues();
+
+        public frmTickers(int Portfolio, int Ticker, string sTicker)
+        {
+            InitializeComponent();
+            PortfolioID = Portfolio;
+            TickerID = Ticker;
+            this.Text = (string.IsNullOrEmpty(sTicker) ? "New Ticker" : sTicker) + " Properties";
+            txtSymbol.Text = sTicker;
+            gbActivity.Text = sTicker + " Activity";
+        }
+
+        private void frmTickers_Load(object sender, EventArgs e)
+        {
+            if (SQL.Connection == ConnectionState.Closed)
+            {
+                DialogResult = DialogResult.Cancel;
+                return;
+            }
+            
+            Pasted = false;
+
+            LoadAADropDown();
+            LoadTicker();
+
+            dsTicker.Tables.Add(SQL.ExecuteDataset(Queries.Ticker_GetTradesDataset(PortfolioID, TickerID)));
+            dgTickers.DataSource = dsTicker.Tables[0];
+
+            dsTicker.AcceptChanges();
+
+            if (TickerID == -1)
+            {
+                txtSymbol.Enabled = true;
+                btnSplit.Visible = false;
+                btnDividends.Visible = false;
+            }
+
+            _TickerReturnValues.MinDate = DateTime.Today;
+            _TickerReturnValues.Changed = chkCalc.Checked;
+            foreach (DataRow dr in dsTicker.Tables[0].Rows)
+                if (Convert.ToDateTime(dr["Date"]) < _TickerReturnValues.MinDate)
+                    _TickerReturnValues.MinDate = Convert.ToDateTime(dr["Date"]);
+
+        }
+
+        private void LoadTicker()
+        {
+            SqlCeResultSet rs = SQL.ExecuteResultSet(Queries.Ticker_GetAttributes(PortfolioID, TickerID));
+            try
+            {
+                if (!rs.HasRows)
+                    return;
+
+                rs.ReadFirst();
+                cmbAA.SelectedValue = rs.GetInt32(rs.GetOrdinal("AA"));
+                if (cmbAA.SelectedValue == null)
+                    cmbAA.SelectedValue = -1;
+                chkCalc.Checked = rs.GetSqlBoolean(rs.GetOrdinal("Active")).IsTrue;
+                chkHide.Checked = rs.GetSqlBoolean(rs.GetOrdinal("Hide")).IsTrue;
+            }
+            finally
+            {
+                rs.Close();
+            }
+        }
+
+        private void LoadAADropDown()
+        {
+            SqlCeResultSet rs = SQL.ExecuteResultSet(Queries.Common_GetAA(PortfolioID));
+            try
+            {
+
+                DataTable t = new DataTable();
+                t.Columns.Add("Display");
+                t.Columns.Add("Value");
+
+                t.Rows.Add("(Blank)", -1);
+
+                if (rs.HasRows)
+                {
+                    int ordName = rs.GetOrdinal("AA");
+                    int ordID = rs.GetOrdinal("ID");
+
+                    rs.ReadFirst();
+                    do
+                    {
+                        t.Rows.Add(rs.GetString(ordName), rs.GetInt32(ordID));
+                    }
+                    while (rs.Read());
+                }
+
+                cmbAA.DisplayMember = "Display";
+                cmbAA.ValueMember = "Value";
+                cmbAA.DataSource = t;
+
+                cmbAA.SelectedValue = -1;
+            }
+            finally
+            {
+                rs.Close();
+            }
+        }
+
+        private void dgTickers_DefaultValuesNeeded(object sender, DataGridViewRowEventArgs e)
+        {
+            e.Row.Cells[0].Value = DateTime.Today;
+        }
+
+        private void dgTickers_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (!(e.Control && e.KeyCode == Keys.V))
+                return;
+            
+            Pasted = true;
+
+            string s = Clipboard.GetText();
+            s = s.Replace("\r", "");
+            string[] lines = s.Split('\n');
+            int row = dgTickers.CurrentCell.RowIndex;
+            int origrow = dgTickers.CurrentCell.RowIndex;
+            int col = dgTickers.CurrentCell.ColumnIndex;
+
+            dgTickers.CancelEdit();
+            dsTicker.AcceptChanges();
+
+            foreach (string line in lines)
+            {
+                if (line == "")
+                    continue;
+
+                string[] cells = line.Split('\t');
+                if (row < dgTickers.Rows.Count - 1)
+                {
+                    for (int i = 0; i < cells.Length; i++)
+                    {
+                        if (col + i < 3)
+                        {
+                            try
+                            {
+                                switch (col + i)
+                                {
+                                    case 0:
+                                        dsTicker.Tables[0].Rows[row]["Date"] = Convert.ToDateTime(cells[i]);
+                                        break;
+                                    case 1:
+                                        dsTicker.Tables[0].Rows[row]["Shares"] = Convert.ToDecimal(cells[i]);
+                                        break;
+                                    case 2:
+                                        dsTicker.Tables[0].Rows[row]["Price"] = Convert.ToDecimal(cells[i]);
+                                        break;
+                                }
+                            }
+                            catch (System.FormatException)
+                            {
+                                // do nothing
+                            }
+                            catch (System.OverflowException)
+                            {
+                                // do nothing
+                            }
+                            dsTicker.AcceptChanges();
+                        }
+                    }
+                    row++;
+
+                }
+                else
+                {
+                    if (cells.Length == dgTickers.Columns.Count)
+                    {
+                        try
+                        {
+                            dsTicker.Tables[0].Rows.Add(Convert.ToDateTime(cells[0]),
+                                                        Convert.ToDecimal(cells[1]),
+                                                        Convert.ToDecimal(cells[2]));
+
+                            row++;
+                        }
+                        catch (System.FormatException)
+                        {
+                            // do nothing
+                        }
+                        catch (System.OverflowException)
+                        {
+                            // do nothing
+                        }
+                        dsTicker.AcceptChanges();
+                    }
+                }
+            }
+              
+            dgTickers.CurrentCell = dgTickers[col, origrow];
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtSymbol.Text))
+            {
+                MessageBox.Show("Set a symbol before saving!");
+                return;
+            }
+            
+            _TickerReturnValues.Changed = TickerReturnValues.Changed != chkCalc.Checked;
+            if (TickerID == -1)
+            {
+                SQL.ExecuteNonQuery(Queries.Ticker_InsertNewTicker(PortfolioID, txtSymbol.Text, Convert.ToInt32(((DataRowView)cmbAA.SelectedItem)["Value"]), chkHide.Checked, chkCalc.Checked));
+                TickerID = Convert.ToInt32(SQL.ExecuteScalar(Queries.Common_GetIdentity()));
+            }
+            else
+            {
+                SQL.ExecuteNonQuery(Queries.Ticker_UpdateTicker(PortfolioID, TickerID, Convert.ToInt32(((DataRowView)cmbAA.SelectedItem)["Value"]), chkHide.Checked, chkCalc.Checked));
+                if (dsTicker.HasChanges() || Pasted)
+                    SQL.ExecuteNonQuery(Queries.Common_DeleteTickerTrades(PortfolioID, TickerID));
+            }
+        
+            if (dsTicker.HasChanges() || Pasted)
+            {
+                _TickerReturnValues.Changed = true;
+                dsTicker.AcceptChanges();
+
+                SqlCeResultSet rs = SQL.ExecuteTableUpdate("Trades");
+                SqlCeUpdatableRecord newRecord = rs.CreateRecord();
+
+                int ordDate = rs.GetOrdinal("Date");
+                int ordShares = rs.GetOrdinal("Shares");
+                int ordPrice = rs.GetOrdinal("Price");
+                int ordPortfolio = rs.GetOrdinal("Portfolio");
+                int ordTickerID = rs.GetOrdinal("TickerID");
+                int ordID = rs.GetOrdinal("ID");
+                int ordTicker = rs.GetOrdinal("Ticker");
+
+                int i = 0;
+
+                try
+                {
+                    foreach (DataRow dr in dsTicker.Tables[0].Rows)
+                    {
+                        newRecord.SetDateTime(ordDate, Convert.ToDateTime(dr["Date"]));
+                        newRecord.SetDecimal(ordShares, Convert.IsDBNull(dr["Shares"]) ? 0 : Convert.ToDecimal(dr["Shares"]));
+                        newRecord.SetDecimal(ordPrice, Convert.IsDBNull(dr["Price"]) ? 0 : Convert.ToDecimal(dr["Price"]));
+                        newRecord.SetInt32(ordPortfolio, PortfolioID);
+                        newRecord.SetInt32(ordTickerID, TickerID);
+                        newRecord.SetInt32(ordID, i);
+                        newRecord.SetString(ordTicker, txtSymbol.Text);
+                        rs.Insert(newRecord);
+
+                        i++;
+                        if (Convert.ToDateTime(dr["Date"]) < TickerReturnValues.MinDate)
+                            _TickerReturnValues.MinDate = Convert.ToDateTime(dr["Date"]);
+                    }
+                }
+                finally
+                {
+                    rs.Close();
+                }
+            }
+            DialogResult = DialogResult.OK;
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+        }
+
+        private void frmTickers_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            SQL.Dispose();
+        }
+
+        private void btnSplit_Click(object sender, EventArgs e)
+        {
+            SqlCeResultSet rs = SQL.ExecuteResultSet(Queries.Ticker_GetSplits(txtSymbol.Text));
+
+            try
+            {
+                if (!rs.HasRows)
+                    MessageBox.Show("Currently no splits exist for this symbol.");
+                else
+                {
+                    int ordDate = rs.GetOrdinal("Date");
+                    int ordRatio = rs.GetOrdinal("Ratio");
+
+                    rs.ReadFirst();
+                    string Message = txtSymbol.Text + " has the following splits:";
+                    do
+                    {
+                        Message = Message + "\n" + rs.GetDateTime(ordDate).ToShortDateString() + " - " + ((double)rs.GetDecimal(ordRatio)).ToString() + ":1";
+                    }
+                    while (rs.Read());
+                    using (frmMsgBox f = new frmMsgBox("Splits", Message))
+                        f.ShowDialog();
+                }
+            }
+            finally
+            {
+                rs.Close();
+            }
+        }
+
+        private void btnDividends_Click(object sender, EventArgs e)
+        {
+            SqlCeResultSet rs = SQL.ExecuteResultSet(Queries.Ticker_GetDividends(txtSymbol.Text));
+
+            try
+            {
+                if (!rs.HasRows)
+                    MessageBox.Show("Currently there have been no dividends for this symbol.");
+                else
+                {
+                    int ordDate = rs.GetOrdinal("Date");
+                    int ordAmount = rs.GetOrdinal("Amount");
+
+                    rs.ReadFirst();
+                    string Message = txtSymbol.Text + " has the following dividends:";
+                    do
+                    {
+                        Message = Message + "\n" + rs.GetDateTime(ordDate).ToShortDateString() + " - " + ((double)rs.GetDecimal(ordAmount)).ToString("C");
+                    }
+                    while (rs.Read());
+                    using (frmMsgBox f = new frmMsgBox("Dividends", Message))
+                        f.ShowDialog();
+                }
+            }
+            finally
+            {
+                rs.Close();
+            }
+        }
+
+        private void dgTickers_DataError(object sender, DataGridViewDataErrorEventArgs e)
+        {
+            MessageBox.Show("Invalid format, must be a number!", "Invalid Data", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+    }
+}

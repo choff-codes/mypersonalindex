657f53e18a5203b6e5d4ca859174625305397885
mypersonalindex <mypersonalindex@724262da-e012-11dd-ab27-75d9513d8d6c>
2009-01-23 04:01:35 +0000

    Separated out general classes

---
+++ branches/1.0.0/MyPersonalIndex/Classes/Functions.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Reflection;
+
+namespace MyPersonalIndex
+{
+    class Functions
+    {
+
+        public static string SQLCleanString(string s)
+        {
+            return s.Replace("'", "''");
+        }
+
+        public static string StripSignifyPortfolio(string Ticker)
+        {
+            return Ticker.Substring(Constants.SignifyPortfolio.Length, Ticker.Length - Constants.SignifyPortfolio.Length);
+        }
+
+        public static Color GetRandomColor(int Seed)
+        {
+            PropertyInfo[] Properties;
+            List<Color> Colors;
+            Color c;
+
+            Properties = typeof (Color).GetProperties (BindingFlags.Public | BindingFlags.Static);
+            Colors = new List<Color>();
+            foreach (PropertyInfo prop in Properties)
+            {
+                c = (Color)prop.GetValue(null, null);
+                if (c == Color.Transparent || c == Color.Empty)
+                    continue;
+                if (c.B + c.A + c.G > 700)
+                    continue;
+                Colors.Add(c);
+            }
+
+            while (Seed * 3 > Colors.Count - 1)
+            {
+                Seed = Seed * 3 - Colors.Count - 1;
+            }
+            
+            return Colors[Seed * 3];
+        }
+    }
+}

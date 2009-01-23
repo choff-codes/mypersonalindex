77d5362a0922c850d3dbbd023cc6f5e62cb7b0d7
mypersonalindex <mypersonalindex@724262da-e012-11dd-ab27-75d9513d8d6c>
2009-01-23 04:01:35 +0000

    Separated out general classes

---
+++ MyPersonalIndex/Classes/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyPersonalIndex
+{
+    public class Constants
+    {
+        public const string SignifyPortfolio = "~|";
+        public enum AvgShareCalc { FIFO, LIFO, AVG };
+        public enum OutputFormat { Currency, Percentage, Decimal, Integer, ShortDate, LongDate, None };
+    }
+}

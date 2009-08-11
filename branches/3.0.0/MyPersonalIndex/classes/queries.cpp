80a8e7828010543a0c00b873afc250e0aae7a4e6
mypersonalindex <mypersonalindex@724262da-e012-11dd-ab27-75d9513d8d6c>
2009-08-11 03:11:04 +0000

    Added database functions

--- branches/3.0.0/MyPersonalIndex/classes/queries.cpp
+++ branches/3.0.0/MyPersonalIndex/classes/queries.cpp
@@ -4,9 +4,49 @@ queries::queries()
 {
     QSettings cfg(QSettings::IniFormat, QSettings::UserScope, "MyPersonalIndex", "MPI");
     QString appDir = QFileInfo(cfg.fileName()).absolutePath().append("/MPI.sqlite");
-    cn = QSqlDatabase::addDatabase("QSQLITE");
-    cn.setDatabaseName(appDir);
-    if (cn.open())
+
+    if (QSqlDatabase::contains(appDir))
+        db = QSqlDatabase::database(appDir);
+    else
     {
+        db = QSqlDatabase::addDatabase("QSQLITE", appDir);
+        db.setDatabaseName(appDir);
+        db.open();
+        QSqlQuery("SELECT load_extension('libsqlitefunctions.so')", db).exec();
     }
+}
+
+QSqlQuery* queries::prepareQuery(queryInfo &q)
+{
+    QSqlQuery *query = new QSqlQuery(db);
+    query->setForwardOnly(true);
+    query->prepare(q.sql);
+
+    foreach (parameter *p, q.parameters)
+        query->bindValue(p->name, p->value);
+
+    query->exec();
+    qDeleteAll(q.parameters);
+
+    return query;
+}
+
+QSqlQuery* queries::executeNonQuery(queryInfo &q)
+{
+    QSqlQuery *query = prepareQuery(q);
+
+    if (query && query->isActive())
+        return query;
+
+    return 0;
+}
+
+QVariant queries::executeScalar(queryInfo &q, const QVariant &nullValue)
+{
+    QSqlQuery *query = prepareQuery(q);
+
+    if (query && query->isActive() && query->next())
+        return query->value(0);
+
+    return nullValue;
 }

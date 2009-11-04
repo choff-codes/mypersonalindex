489f350bca6220fc59913ba24f974fced77b2a01
mypersonalindex <mypersonalindex@724262da-e012-11dd-ab27-75d9513d8d6c>
2009-11-04 13:27:54 +0000

    Date calc updates

--- MyPersonalIndex/classes/NAV.h
+++ MyPersonalIndex/classes/NAV.h
@@ -39,7 +39,7 @@ private:
     bool getCurrentDateOrNext(int &date);
     QMap<int, QList<globals::dynamicTrade> > getTrades(const int &portfolioID, const int &minDate);
     QList<int> getOnceTrades(const globals::dynamicTrade &d, const int &minDate, const int &maxDate);
-    QList<int> getDailyTrades(const globals::dynamicTrade&, const int &minDate, const int &maxDate);
+    //QList<int> getDailyTrades(const globals::dynamicTrade&, const int &minDate, const int &maxDate);
     QList<int> getWeeklyTrades(const globals::dynamicTrade &d, const int &minDate, const int &maxDate);
     QList<int> getMonthlyTrades(const globals::dynamicTrade &d, const int &minDate, const int &maxDate);
     QList<int> getYearlyTrades(const globals::dynamicTrade &d, const int &minDate, const int &maxDate);

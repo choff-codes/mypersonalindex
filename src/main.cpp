2ecb735e95dd92a559fc12bc5f4c93c954706f2d
mypersonalindex <mypersonalindex@724262da-e012-11dd-ab27-75d9513d8d6c>
2010-10-17 05:02:18 +0000

    Creation of the main window

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,12 @@
-#include <QMessageBox>
-#include <QString>
-#include <QDir>
 #include <QApplication>
-//#include <QtSingleApplication>
-//#include "frmMain.h"
-//#include "portfolioFactory.h"
-#include "frmEdit.h"
+#include "frmMain.h"
 
 int main(int argc, char *argv[])
 {
-    //QtSingleApplication app("MyPersonalIndexApp", argc, argv);
     QApplication app(argc, argv);
 
-//    if (app.isRunning())
-//    {
-//        QMessageBox::information(0, "My Personal Index", "Only one instance of My Personal Index can be run at a time!");
-//        return 0;
-//     }
+    frmMain form;
+    form.show();
 
-//    QString location = queries::getDefaultDatabaseLocation();
-//    if (!QFile::exists(location))
-//        if (!QDir().mkpath(QFileInfo(location).absolutePath()) ||
-//            !QFile::copy(QCoreApplication::applicationDirPath().append("/MPI.sqlite"), location))
-//        {
-//            QMessageBox::critical(0, "Error", "Cannot write to the user settings folder!", QMessageBox::Ok);
-//            QApplication::quit();
-//            return 0;
-//        }
-//
-
-    frmEdit(portfolio()).exec();
-    //app.setActivationWindow(&mainForm);
-    //form.show();
-
-
-    //QMap<int, portfolio> portfolios = portfolioFactory(queries(queries::getDefaultDatabaseLocation())).getPortfolios(true);
     return app.exec();
 }
--- src/ui/frmMain_UI.h
+++ src/ui/frmMain_UI.h
@@ -1,156 +1,53 @@
 #ifndef FRMMAIN_UI_H
 #define FRMMAIN_UI_H
 
-#include <QHBoxLayout>
-#include <QToolBar>
-#include <QAction>
-#include <QLabel>
-#include <QComboBox>
-#include <QStatusBar>
-#include <QProgressBar>
 #include <QMainWindow>
-#include <QShortcut>
-#include <qwt_plot_grid.h>
-#include <qwt_plot_zoomer.h>
-#include "qwt_plot_layout.h"
-#include "qwt_plot_canvas.h"
-#include "qwt_text_engine.h"
-#include "mpiDateEdit.h"
-#include "mpiTableView.h"
-#include "mpiChart.h"
-#include "mpiDateScale.h"
-#include "mpiPercentScale.h"
+#include <QAction>
+#include <QMenu>
+#include <QMenuBar>
+#include <QComboBox>
+#include <QLabel>
+#include <QHBoxLayout>
 
 class frmMain_UI
 {
 public:
-    static const QString LAST_UPDATED_TEXT;
-    static const QString INDEX_START_TEXT;
-    static const QString STATUS_TEXT;
-    static const QString DATE;
-    static const QString START_DATE;
-    static const QString END_DATE;
-    static const QString WINDOW_TITLE;
-    static const QString BUSY;
-
-    QWidget *centralwidget;
-    QHBoxLayout *centralgrid;
-
-    QToolBar *mainToolbar;
-    QAction *mainUpdatePrices;
-    QAction *mainCompare;
-    QAction *mainEdit;
-    QAction *mainImport;
-    QAction *mainDelete;
-    QAction *mainAdd;
-    QAction *mainOptions;
-    QAction *mainAbout;
-    QLabel *mainPortfolioLabel;
-    QComboBox *mainPortfolioCombo;
-
-    QVBoxLayout *holdingsGrid;
-    QToolBar *holdingsToolbar;
-    QAction *holdingsAdd;
-    QAction *holdingsEdit;
-    QAction *holdingsDelete;
-    QAction *holdingsShowHidden;
-    QAction *holdingsReorderColumns;
-    QAction *holdingsExport;
-    QLabel *holdingsDate;
-    mpiDateEdit *holdingsDateDropDown;
-    QLabel *holdingsSortLabel;
-    QComboBox *holdingsSortCombo;
-    mpiTableView *holdings;
-    QShortcut *holdingsCopyShortcut;
-
-    QVBoxLayout *statGrid;
-    QToolBar *statToolbar;
-    QAction *statEdit;
-    QAction *statExport;
-    QLabel *statStartDate;
-    mpiDateEdit *statStartDateDropDown;
-    QLabel *statEndDate;
-    mpiDateEdit *statEndDateDropDown;
-    mpiTableView *stat;
-    QShortcut *statCopyShortcut;
-
-    QVBoxLayout *chartGrid;
-    QToolBar *chartToolbar;
-    QAction *chartExport;
-    QLabel *chartStartDate;
-    mpiDateEdit *chartStartDateDropDown;
-    QLabel *chartEndDate;
-    mpiDateEdit *chartEndDateDropDown;
-    mpiChart *chart;
-    QwtPlotGrid *chartGridLines;
-    QwtPlotZoomer *chartZoomer;
-
-    QVBoxLayout *performanceGrid;
-    QToolBar *performanceToolbar;
-    QAction *performanceSortDesc;
-    QAction *performanceExport;
-    mpiTableView *performance;
-    QShortcut *performanceCopyShortcut;
-
-    QVBoxLayout *correlationsGrid;
-    QToolBar *correlationsToolbar;
-    QAction *correlationsShowHidden;
-    QAction *correlationsExport;
-    QLabel *correlationsStartDate;
-    mpiDateEdit *correlationsStartDateDropDown;
-    QLabel *correlationsEndDate;
-    mpiDateEdit *correlationsEndDateDropDown;
-    mpiTableView *correlations;
-    QShortcut *correlationsCopyShortcut;
-
-    QVBoxLayout *accountsGrid;
-    QToolBar *accountsToolbar;
-    QAction *accountsAdd;
-    QAction *accountsEdit;
-    QAction *accountsDelete;
-    QAction *accountsShowBlank;
-    QAction *accountsReorderColumns;
-    QAction *accountsExport;
-    QLabel *accountsDate;
-    mpiDateEdit *accountsDateDropDown;
-    QLabel *accountsSortLabel;
-    QComboBox *accountsSortCombo;
-    mpiTableView *accounts;
-    QShortcut *accountsCopyShortcut;
-
-    QVBoxLayout *aaGrid;
-    QToolBar *aaToolbar;
-    QAction *aaAdd;
-    QAction *aaEdit;
-    QAction *aaDelete;
-    QAction *aaShowBlank;
-    QAction *aaReorderColumns;
-    QAction *aaExport;
-    QLabel *aaDate;
-    mpiDateEdit *aaDateDropDown;
-    QLabel *aaSortLabel;
-    QComboBox *aaSortCombo;
-    mpiTableView *aa;
-    QShortcut *aaCopyShortcut;
-
-    QTabWidget *tab;
-    QWidget *tab_holdings;
-    QWidget *tab_stat;
-    QWidget *tab_chart;
-    QWidget *tab_performance;
-    QWidget *tab_correlations;
-    QWidget *tab_accounts;
-    QWidget *tab_aa;
-
-    QStatusBar *status;
-    QLabel *stbLastUpdated;
-    QLabel *stbStartDate;
-    QLabel *stbStatus;
-    QProgressBar *stbProgress;
+    QMenu *file;
+    QAction *fileNew;
+    QAction *fileOpen;
+    QMenu *fileRecent;
+    QAction *fileClose;
+    QAction *fileSave;
+    QAction *fileSaveAs;
+    QAction *fileExit;
+    QMenu *view;
+    QAction *viewSummary;
+    QAction *viewTrades;
+    QAction *viewAccounts;
+    QAction *viewAssetAllocation;
+    QAction *viewSecurities;
+    QAction *viewStatistics;
+    QAction *viewCharts;
+    QAction *viewCorrelations;
+    QAction *viewPerformance;
+    QMenu *portfolio;
+    QAction *portfolioAdd;
+    QAction *portfolioEdit;
+    QAction *portfolioDelete;
+    QMenu *import;
+    QAction *importYahoo;
+    QAction *importPrices;
+    QAction *importPortfolio;
+    QAction *importFile;
+    QMenu *help;
+    QAction *helpAbout;
+    QMenuBar *menubar;
+    QWidget *portfolioDropDownWidget;
+    QHBoxLayout *portfolioDropDownLayout;
+    QLabel *portfolioDropDown;
+    QComboBox *portfolioDropDownCmb;
 
     void setupUI(QMainWindow *mainWindow);
-
-    ~frmMain_UI() { delete chartGridLines; delete chartZoomer; }
 };
 
 #endif // FRMMAIN_UI_H
